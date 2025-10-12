e95331e1461d156f60dd27a3734438d2b8e82c8d
mishandri <mishandri@gmail.com>
2025-10-12 20:18:33 +0300

    Тест преобразования ответа от API в датафрейм

--- get_data.py
+++ get_data.py
@@ -1,5 +1,7 @@
 from datetime import date, timedelta
+import pandas as pd
 import requests
+import json
 
 api_url = "http://final-project.simulative.ru/data"
 params = {
@@ -7,4 +9,12 @@ params = {
 }
 
 data = requests.get(api_url, params)
-print(data.json())
+
+df = pd.DataFrame(data.json())     # Считываем данные
+df = df.rename(columns={'purchase_datetime': 'purchase_dt'})    # Переименуем колонку, чтобы потом её удалить
+df['purchase_datetime'] = pd.to_datetime(df['purchase_dt']) + \
+    pd.to_timedelta(df['purchase_time_as_seconds_from_midnight'], unit='s') # Преобразуем две колонки с датой и временем в одну
+df = df.drop(['purchase_dt', 'purchase_time_as_seconds_from_midnight'], axis=1) # Удаляем старые колонки с датой и временем
+print(df)
+
+
