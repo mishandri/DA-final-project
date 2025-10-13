dd79f18c8fe6a9c4b4f6c8bc5f91be62a20be648
mishandri <mishandri@gmail.com>
2025-10-13 17:11:25 +0300

    Выделение функции забора данных в отдельный файл

---
+++ fetch_data.py
@@ -0,0 +1,19 @@
+from datetime import date, datetime, timedelta
+import pandas as pd
+import requests
+import json
+
+def fetch_data (api_url:str, date:str)->pd.DataFrame:
+    """Функция делает запрос к API по api_url и 
+    возвращает датафрейм данных за дату date"""
+    params = {
+        "date":date
+    }
+    response = requests.get(api_url, params) # делаем запрос
+    df = pd.DataFrame(response.json())     # Считываем данные
+    df = df.rename(columns={'purchase_datetime': 'purchase_dt'})    # Переименуем колонку, чтобы потом её удалить
+    df['purchase_datetime'] = pd.to_datetime(df['purchase_dt']) + \
+        pd.to_timedelta(df['purchase_time_as_seconds_from_midnight'], unit='s') # Преобразуем две колонки с датой и временем в одну
+    df = df.drop(['purchase_dt', 'purchase_time_as_seconds_from_midnight'], axis=1) # Удаляем старые колонки с датой и временем
+    print(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Данные загружены за {date}")
+    return df
